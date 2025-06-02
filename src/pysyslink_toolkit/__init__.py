a9fa40b1fbbc256904bad0f87f7ce4af6edab528
Pello Usabiaga <pellousabiaga@gmail.com>
2025-06-02 21:36:01 +0200

    import api to toolkit

--- src/pysyslink_toolkit/__init__.py
+++ src/pysyslink_toolkit/__init__.py
@@ -2,3 +2,4 @@ from .BlockRenderInformation import BlockRenderInformation
 from .HighLevelBlock import HighLevelBlock
 from .LowLevelBlockStructure import LowLevelBlockStructure
 from .Plugin import Plugin
+from .api import *
